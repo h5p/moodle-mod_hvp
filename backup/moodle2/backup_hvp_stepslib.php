918324814776601b2b7964f0865577307d62e251
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2016-03-11 17:16:40 +0200

    Implements preliminary version of Moodle's backup feature

--- backup/moodle2/backup_hvp_stepslib.php
+++ backup/moodle2/backup_hvp_stepslib.php
@@ -15,68 +15,107 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package moodlecore
- * @subpackage backup-moodle2
- * @copyright 2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @package mod_hvp
+ * @copyright 2016 Mediamaisteri Oy {@link http://www.mediamaisteri.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+require_once($CFG->dirroot . '/backup/moodle2/backup_activity_task.class.php');
+
 /**
- * Define all the backup steps that will be used by the backup_choice_activity_task
+ * Define all the backup steps that will be used by the backup_hvp_activity_task
  */
 
 /**
- * Define the complete choice structure for backup, with file and id annotations
+ * Define the complete hvp structure for backup, with file and id annotations
  */
 class backup_hvp_activity_structure_step extends backup_activity_structure_step {
 
     protected function define_structure() {
-        /*
+
         // To know if we are including userinfo
         $userinfo = $this->get_setting_value('userinfo');
 
         // Define each element separated
-        $choice = new backup_nested_element('choice', array('id'), array(
-            'name', 'intro', 'introformat', 'publish',
-            'showresults', 'display', 'allowupdate', 'showunanswered',
-            'limitanswers', 'timeopen', 'timeclose', 'timemodified',
-            'completionsubmit'));
+        $hvp = new backup_nested_element('hvp', array('id'), array(
+            'course',
+            'name',
+            'json_content',
+            'embed_type',
+            'disable',
+            'main_library_id',
+            'content_type',
+            'author',
+            'license',
+            'meta_keywords',
+            'meta_description',
+            'filtered',
+            'slug',
+            'timecreated',
+            'timemodified',
+        ));
 
-        $options = new backup_nested_element('options');
+        $hvp_libraries = new backup_nested_element('libraries');
+        $hvp_library = new backup_nested_element('library', null, array(
+            'machine_name',
+            'major_version',
+            'minor_version',
+            'patch_version',
+            'dependency_type',
+            'drop_css',
+            'weight',
+        ));
 
-        $option = new backup_nested_element('option', array('id'), array(
-            'text', 'maxanswers', 'timemodified'));
-
-        $answers = new backup_nested_element('answers');
-
-        $answer = new backup_nested_element('answer', array('id'), array(
-            'userid', 'optionid', 'timemodified'));
+        $content_user_data_entries = new backup_nested_element('content_user_data');
+        $content_user_data = new backup_nested_element('entry', array('id'), array(
+            'user_id',
+            'hvp_id',
+            'sub_content_id',
+            'data_id',
+            'data',
+            'preloaded',
+            'delete_on_content_change',
+        ));
 
         // Build the tree
-        $choice->add_child($options);
-        $options->add_child($option);
+        $hvp->add_child($content_user_data_entries);
+        $content_user_data_entries->add_child($content_user_data);
 
-        $choice->add_child($answers);
-        $answers->add_child($answer);
+        $hvp->add_child($hvp_libraries);
+        $hvp_libraries->add_child($hvp_library);
 
         // Define sources
-        $choice->set_source_table('choice', array('id' => backup::VAR_ACTIVITYID));
+        $hvp->set_source_table('hvp', array('id' => backup::VAR_ACTIVITYID));
 
-        $option->set_source_table('choice_options', array('choiceid' => backup::VAR_PARENTID), 'id ASC');
+        $lib_sql = "SELECT
+                machine_name,
+                major_version,
+                minor_version,
+                patch_version,
+                dependency_type,
+                drop_css,
+                weight
+            FROM {hvp_libraries} hl
+            JOIN {hvp_contents_libraries} hcl
+                ON hl.id = hcl.library_id
+            JOIN {hvp} h
+                ON h.id = hcl.hvp_id
+            WHERE h.id = ?";
+
+        $hvp_library->set_source_sql($lib_sql, array(backup::VAR_ACTIVITYID));
 
         // All the rest of elements only happen if we are including user info
         if ($userinfo) {
-            $answer->set_source_table('choice_answers', array('choiceid' => '../../id'));
+            $content_user_data->set_source_table('hvp_content_user_data', array('hvp_id' => backup::VAR_PARENTID), 'id ASC');
         }
 
         // Define id annotations
-        $answer->annotate_ids('user', 'userid');
+        $content_user_data->annotate_ids('user', 'user_id');
 
         // Define file annotations
-        $choice->annotate_files('mod_choice', 'intro', null); // This file area hasn't itemid
+        $hvp->annotate_files('mod_hvp', 'content', null);
 
-        // Return the root element (choice), wrapped into standard activity structure
-        return $this->prepare_activity_structure($choice);
-        */
+        // Return the root element (hvp), wrapped into standard activity structure
+        return $this->prepare_activity_structure($hvp);
     }
 }
