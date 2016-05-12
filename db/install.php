84cc6793e9d9f56dc79887e633a4819f38ae7e42
Thomas Marstrander <thomasmars87@gmail.com>
2016-05-12 11:59:39 +0200

    Added welcome message when installing H5P.

---
+++ db/install.php
@@ -0,0 +1,24 @@
+<?php
+
+function xmldb_hvp_install() {
+    echo '<h3>Welcome to the world of H5P!</h3>' .
+         '<p>' .
+            'To get started with H5P and Moodle take a look at our ' .
+            '<a href="https://h5p.org/moodle" target="_blank">tutorial</a>' .
+            ' and check out the ' .
+            '<a href="https://h5p.org/content-types-and-applications" target="_blank">example content</a>' .
+            ' at H5P.org for inspiration.' .
+         '</p>' .
+         '<p>' .
+             'We hope you will enjoy H5P and get engaged in our growing community through our ' .
+             '<a href="https://h5p.org/forum" target="_blank">forums</a>' .
+             ' and chat room ' .
+             '<a href="https://gitter.im/h5p/CommunityChat" target="_blank">gitter.im/h5p/CommunityChat</a>' .
+         '</p>' .
+         '<p>' .
+            'If you have any feedback, don\'t hesitate to ' .
+            '<a href="https://h5p.org/contact" target="_blank">contact us</a>. ' .
+            'We take feedback very seriously and are dedicated ' .
+            'to making H5P better every day!' .
+         '</p>';
+}
