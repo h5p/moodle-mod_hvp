7e9d0033bfa34b211dd9613a6e100fd7b9fdf7f3
Frode Petterson <frode.andre.petterson@gmail.com>
2016-07-18 10:53:19 +0200

    Corrected indentation

--- view.php
+++ view.php
@@ -83,14 +83,14 @@ if (!isset($CFG->mod_hvp_export) || $CFG->mod_hvp_export === true) {
     // Find course context.
     $context = \context_course::instance($course->id);
     if (has_capability('mod/hvp:getexport', $context)) {
-      $hvppath = "{$CFG->httpswwwroot}/pluginfile.php/{$context->id}/mod_hvp";
-      $exportfilename = ($content['slug'] ? $content['slug'] . '-' : '') . $content['id'] . '.h5p';
-      $export = "{$hvppath}/exports/{$exportfilename}";
+        $hvppath = "{$CFG->httpswwwroot}/pluginfile.php/{$context->id}/mod_hvp";
+        $exportfilename = ($content['slug'] ? $content['slug'] . '-' : '') . $content['id'] . '.h5p';
+        $export = "{$hvppath}/exports/{$exportfilename}";
     }
 }
 if (empty($export)) {
-  // Remove Download button when there's no export URL
-  $content['disable'] |= \H5PCore::DISABLE_DOWNLOAD;
+    // Remove Download button when there's no export URL
+    $content['disable'] |= \H5PCore::DISABLE_DOWNLOAD;
 }
 
 // Add JavaScript settings for this content.
