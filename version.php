9c2bc91a57d868f58b7ae51bf7a96582eddc1ef2
Sara Cenni <sara.cenni@unibo.it>
2016-08-04 17:10:40 +0200

    fix(hvp): alzato il numero di versione per evitare problemi con la capability (https://github.com/h5p/h5p-moodle-plugin/issues/101)

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016051306;
+$plugin->version   = 2016051307;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
