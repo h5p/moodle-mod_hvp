d6e7809a2e4ac82db59bc886af0d65861dede891
thomasmars <thomasmars87@gmail.com>
2016-11-30 11:49:53 +0100

    Increased version, prepaed for release 1.0-rc.5

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016110100;
+$plugin->version   = 2016113000;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_RC;
-$plugin->release   = '1.0-rc.4';
+$plugin->release   = '1.0-rc.5';
