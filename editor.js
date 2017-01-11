e29973d8bcd1d4da4a0ab11425739a98b1e78397
Tom Arild Jakobsen <tajakobsen@gmail.com>
2017-01-11 11:31:44 +0100

    Fix hiding file selector in editor, when displaying content creation
    Relates to: HFP-421

--- editor.js
+++ editor.js
@@ -1,14 +1,22 @@
 (function ($) {
-  $(document).ready(function () {
+  function getRow ($el) {
+    return $el.closest('.fitem');
+  }
+
+  function init () {
     var $editor = $('.h5p-editor');
+    var $fileField = $('input[name="h5pfile"]');
+
     H5PEditor.init(
       $('#mform1'),
       $('input[name="h5paction"]'),
-      $('#fitem_id_h5pfile'),
-      $editor.parent().parent(), // Static elements have no custom id or class
+      getRow($fileField),
+      getRow($editor),
       $editor,
       $('input[name="h5plibrary"]'),
       $('input[name="h5pparams"]')
     );
-  });
+  }
+
+  $(document).ready(init);
 })(H5P.jQuery);
