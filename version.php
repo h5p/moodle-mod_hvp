f5f72856115e46e883bee37cc6c53743473e0ea5
Paal Joergensen <pal.jorgensen@gmail.com>
2017-01-24 13:46:10 +0100

    Preparing release - bumping version

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_RC;
-$plugin->release   = '1.0-rc.5';
+$plugin->release   = '1.0-rc.6';
