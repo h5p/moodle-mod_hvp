831c9d4d44644d0884b64f64d9da8b204ec5c16e
thomasmars <thomasmars87@gmail.com>
2017-03-27 16:46:43 +0200

    Include restricted settings in latest libraries info
    HFP-507

--- classes/editor_ajax.php
+++ classes/editor_ajax.php
@@ -49,7 +49,7 @@ class editor_ajax implements \H5PEditorAjaxInterface {
 
         return $DB->get_records_sql("
             SELECT hl4.id AS library_id, hl4.machine_name, hl4.major_version,
-                hl4.minor_version, hl4.patch_version, hl4.has_icon
+                hl4.minor_version, hl4.patch_version, hl4.has_icon, hl4.restricted
             FROM {hvp_libraries} hl4
             JOIN ({$max_minor_version_sql}) hl3
             ON hl4.machine_name = hl3.machine_name
