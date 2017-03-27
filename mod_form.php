cf48ba8a49b8d4beb135f6a9c88f06f7a3d09e28
Thomas <marstranderthomas@gmail.com>
2017-03-27 11:55:21 +0200

    Revert "Make Name field required."

--- mod_form.php
+++ mod_form.php
@@ -35,7 +35,6 @@ class mod_hvp_mod_form extends moodleform_mod {
         // Name.
         $mform->addElement('text', 'name', get_string('name'));
         $mform->setType('name', PARAM_TEXT);
-        $mform->addRule('name', null, 'required', null, 'client');
         $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
 
         // Intro.
