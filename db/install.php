0bdd1daa5014942d381e63a4d20817e27f7a3f02
thomasmars <thomasmars87@gmail.com>
2017-03-30 15:35:20 +0200

    Use correct link for h5p usage tracking
    JI-106

--- db/install.php
+++ db/install.php
@@ -42,7 +42,7 @@ function xmldb_hvp_install() {
          '</p>';
 
     // Notify of communication with H5P Hub
-    echo '<p>H5P fetches content types directly from the H5P Hub. In order to do this the H5P plugin will communicate with the Hub once a day to fetch information about new and updated content types. It will send in anonymous data to the Hub about H5P usage. Read more at <a href="https://h5p.org/h5p-plugin-communication">the plugin communication page at H5P.org</a>.</p>';
+    echo '<p>H5P fetches content types directly from the H5P Hub. In order to do this the H5P plugin will communicate with the Hub once a day to fetch information about new and updated content types. It will send in anonymous data to the Hub about H5P usage. Read more at <a href="https://h5p.org/tracking-the-usage-of-h5p">the plugin communication page at H5P.org</a>.</p>';
 
     \mod_hvp\framework::printMessages('info', \mod_hvp\framework::messages('info'));
     \mod_hvp\framework::printMessages('error', \mod_hvp\framework::messages('error'));
