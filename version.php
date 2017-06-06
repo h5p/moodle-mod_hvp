cb1e5ea73690f8a5f96d906c824e2bc92bc09e17
thomasmars <thomasmars87@gmail.com>
2017-06-06 15:27:01 +0200

    Prepare for release 1.4

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017052400;
+$plugin->version   = 2017060600;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.3';
+$plugin->release   = '1.4';
