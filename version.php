6832888de51c5034369c82d9f1f15846c039bdc9
thomasmars <thomasmars87@gmail.com>
2017-06-27 11:35:58 +0200

    Updated version for regression.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017062301;
+$plugin->version   = 2017062700;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
