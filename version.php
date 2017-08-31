6d615f1b96a0e3e2080c339ec120583df166938c
thomasmars <thomasmars87@gmail.com>
2017-08-31 12:44:57 +0200

    Prepared for release, Version set to 1.5

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017062700;
+$plugin->version   = 2017083100;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.4';
+$plugin->release   = '1.5';
