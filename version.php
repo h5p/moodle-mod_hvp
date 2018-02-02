45c7e33c42e927fc25103a5f84e4f63808b88dbb
thomasmars <thomasmars87@gmail.com>
2018-02-02 11:13:05 +0100

    Prepare for user-testing new version.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018012200;
+$plugin->version   = 2018020200;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
