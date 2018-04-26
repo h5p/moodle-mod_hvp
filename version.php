4b7563f1b26d9afaa6e4aa6a178a3282346c2a9e
thomasmars <thomasmars87@gmail.com>
2018-04-26 12:50:55 +0200

    Prepare version for release

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018020200;
+$plugin->version   = 2018042600;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
