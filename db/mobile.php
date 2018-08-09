9e0a9e846c32feb21c3fe24535ae4dd7d1904fbd
Andres <andrescorredor20@gmail.com>
2018-08-09 17:24:46 +0000

    template to render on mobile app

---
+++ db/mobile.php
@@ -0,0 +1,15 @@
+<?php
+$addons = array(
+    "mod_hvp" => array( // Plugin identifier
+        'handlers' => array( // Different places where the plugin will display content.
+            'coursehvp' => array( // Handler unique name (alphanumeric).
+                'delegate' => 'CoreCourseModuleDelegate', // Delegate (where to display the link to the plugin)
+                'method' => 'mobile_course_view', // Main function in \mod_certificate\output\mobile
+            	'displaydata' => array(
+                	'icon' => $CFG->wwwroot . '/mod/hvp/pix/icon.svg',
+                	'class' => '',
+            	),
+            )
+        )
+    )
+);
