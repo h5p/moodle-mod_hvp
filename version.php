ff44a958360859acea23fbcf0c40d3da1fe06d55
thomasmars <thomasmars87@gmail.com>
2018-11-06 15:20:43 +0100

    Prepare for release 1.12

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018110200;
+$plugin->version   = 2018110600;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.11';
+$plugin->release   = '1.12';
