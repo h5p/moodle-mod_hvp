602806fc4affeca9e9291555dc2eb828193f48b4
Frode Petterson <frode.andre.petterson@gmail.com>
2019-02-05 15:19:39 +0100

    JI-942 Remove unused autoload

--- autoloader.php
+++ autoloader.php
@@ -45,7 +45,6 @@ function hvp_autoloader($class) {
         'H5PDefaultStorage' => 'library/h5p-default-storage.class.php',
         'H5PEventBase' => 'library/h5p-event-base.class.php',
         'H5PMetadata' => 'library/h5p-metadata.class.php',
-        'H5PSaveContentOutdatedLibraryException' => 'library/exceptions/h5p-save-content-outdated-library-exception.class.php',
 
         // Editor.
         'H5peditor' => 'editor/h5peditor.class.php',
