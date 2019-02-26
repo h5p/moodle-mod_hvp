07a11f4496662b078dedaa719195d2476b1dbece
Thomas Marstrander <marstranderthomas@gmail.com>
2019-02-26 10:54:43 +0100

    Add auto-login to embed code when username is provided
    NB: A security mechanism must be added to embed.php

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -10,12 +10,12 @@ use mod_hvp;
 class mobile {
 
   public static function mobile_course_view($args) {
-    global $DB, $CFG;
+    global $DB, $CFG, $OUTPUT, $USER;
 
-    $id = $args['cmid'];
+    $cmid = $args['cmid'];
 
     // Verify course context.
-    $cm = get_coursemodule_from_id('hvp', $id);
+    $cm = get_coursemodule_from_id('hvp', $cmid);
     if (!$cm) {
       print_error('invalidcoursemodule');
     }
@@ -27,37 +27,20 @@ class mobile {
     $context = context_module::instance($cm->id);
     require_capability('mod/hvp:view', $context);
 
-    // Set up view assets.
-    $view = new mod_hvp\view_assets($cm, $course);
-    $view->validatecontent();
-
-    $html = '';
-
-    // Print any messages.
-    // $html .= \mod_hvp\framework::printMessages('info', \mod_hvp\framework::messages('info'));
-    // $html .= \mod_hvp\framework::printMessages('error', \mod_hvp\framework::messages('error'));
-
-    $html .= $view->outputview();
-
-    // TODO: Import js from other file
-    $scripts     = $view->getjsassets();
-    $corescripts = $view->getcorejsassets();
-    $h5pintegration = $view->getSettings();
-
-    $jscontent = 'window.H5PIntegration = ' . json_encode($h5pintegration) . ';';
-    $jscontent .= file_get_contents($CFG->dirroot . '/mod/hvp/library/js/jquery.js');
-    $jscontent .= file_get_contents($CFG->dirroot . '/mod/hvp/library/js/h5p.js');
-//    $jscontent .= file_get_contents($CFG->dirroot . '/mod/hvp/library/js/h5p-event-dispatcher.js');
-
+    $data = [
+      'cmid' => $cmid,
+      'wwwroot' => $CFG->wwwroot,
+      'username' => $USER->username,
+    ];
 
     return array(
       'templates'  => array(
         array(
           'id'   => 'main',
-          'html' => $html,
+          'html' => $OUTPUT->render_from_template('mod_hvp/mobile_view_page',$data ),
         ),
       ),
-      'javascript' => $jscontent,
+      'javascript' => file_get_contents($CFG->dirroot . '/mod/hvp/library/js/h5p-resizer.js'),
     );
   }
 }
--- embed.php
+++ embed.php
@@ -26,6 +26,12 @@ require_once("locallib.php");
 global $PAGE, $DB, $CFG, $OUTPUT;
 
 $id = required_param('id', PARAM_INT);
+// TODO: This needs to be protected by a token or some other security mechanism
+$username = optional_param('username', null, PARAM_ALPHANUMEXT);
+if ($username) {
+  $user = get_complete_user_data('username', $username);
+  complete_user_login($user);
+}
 
 // Verify course context.
 $cm = get_coursemodule_from_id('hvp', $id);
