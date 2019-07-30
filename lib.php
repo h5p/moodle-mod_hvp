135e8cf6eb7ad2fe54e82315df15fb14befcf7f6
Leon Rodney <leon.rodney@warwick.ac.uk>
2019-07-30 13:08:42 +0100

    implements passing grade activity completion #163

--- lib.php
+++ lib.php
@@ -55,7 +55,7 @@ function hvp_supports($feature) {
         case FEATURE_COMPLETION_TRACKS_VIEWS:
             return true;
         case FEATURE_COMPLETION_HAS_RULES:
-            return false;
+            return true;
         case FEATURE_GRADE_HAS_GRADE:
             return true;
         case FEATURE_GRADE_OUTCOMES:
@@ -385,4 +385,40 @@ function hvp_update_grades($hvp=null, $userid=0, $nullifnone=true) {
     } else {
         hvp_grade_item_update($hvp);
     }
+}
+
+/**
+ * Obtains the automatic completion state for this H5P activity on any conditions
+ * in settings, such as if a certain grade is achieved.
+ *
+ * @param object $course Course
+ * @param object $cm Course-module
+ * @param int $userid User ID
+ * @param bool $type Type of comparison (or/and; can be used as return value if no conditions)
+ * @return bool True if completed, false if not. (If no conditions, then return
+ *   value depends on comparison type)
+ */
+function hvp_get_completion_state($course, $cm, $userid, $type) {
+    global $DB, $CFG;
+
+    $hvp = $DB->get_record('hvp', array('id' => $cm->instance), '*', MUST_EXIST);
+
+    if (!$hvp->completionpass) {
+        return $type;
+    }
+
+    // Check for passing grade.
+    if ($hvp->completionpass) {
+        require_once($CFG->libdir . '/gradelib.php');
+        $item = grade_item::fetch(array('courseid' => $course->id, 'itemtype' => 'mod',
+                'itemmodule' => 'hvp', 'iteminstance' => $cm->instance, 'outcomeid' => null));
+
+        if ($item) {
+            $grades = grade_grade::fetch_users_grades($item, array($userid), false);
+            if (!empty($grades[$userid])) {
+                return $grades[$userid]->is_passed($item);
+            }
+        }
+    }
+    return false;
 }
--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019052100;
+$plugin->version   = 2019073000;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
