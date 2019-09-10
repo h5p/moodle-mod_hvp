14ed52d594d67d0175f4f9b5d9f793148dda77be
Michiel Boerman <mbb@id5r.nl>
2019-09-10 15:07:35 +0200

    pull from upstream

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019081600;
+$plugin->version   = 2019081601;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
