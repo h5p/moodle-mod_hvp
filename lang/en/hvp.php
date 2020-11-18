8155e1516275b07485a161f67cc03b06349b2c0d
Thomas Marstrander <marstranderthomas@gmail.com>
2020-11-18 12:56:21 +0100

    Add translation for grading header
    Fixes #322

--- lang/en/hvp.php
+++ lang/en/hvp.php
@@ -477,3 +477,4 @@ $string['completionpassdesc'] = 'Student must achieve a passing grade to complet
 $string['completionpass_help'] = 'If enabled, this activity is considered complete when the student receives a pass grade (as specified in the Grade section of the H5P activity settings) or higher.';
 $string['gradetopassnotset'] = 'This H5P activity does not yet have a grade to pass set. It may be set in the Grade section of the H5P activity settings.';
 $string['gradetopassmustbeset'] = 'Grade to pass cannot be zero as this H5P activity has its completion method set to require passing grade. Please set a non-zero value.';
+$string['gradeheading'] = 'Results for {$a}';
