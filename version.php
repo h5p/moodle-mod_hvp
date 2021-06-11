d236301fef1dc77a305c87066f45920f4c7a1f9b
Thomas Marstrander <marstranderthomas@gmail.com>
2021-06-11 11:04:06 +0200

    Prepare for release 1.22.3

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021043000;
+$plugin->version   = 2021061100;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.22.2';
+$plugin->release   = '1.22.3';
