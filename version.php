5102a4a455450a7c972ca2aec47dfd741191d9c6
Falcon <s.t.g.with@gmail.com>
2023-06-12 09:49:25 +0200

    Update version

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022121200;
+$plugin->version   = 2023061200;
 $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.23.0';
+$plugin->release   = '1.23.2';
